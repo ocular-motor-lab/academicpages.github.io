bb5aad10263e4ab6ec78216908c7178628048a48
Jorge Otero-Millan <jorge.oteromillan@gmail.com>
2021-04-20 23:31:54 -0700

    test

--- assets/notion/NotionToMD.m
+++ assets/notion/NotionToMD.m
@@ -5,17 +5,38 @@ papers(papers.Status ~= 'Published ',:) = [];
 
 for i=1:3%height(papers)
     i
-    lines ={
+%     lines ={
+%         '---'
+%         ['title: "' papers.Title{i} '"']
+%         'collection: publications'
+%         ['permalink: /publication/' sprintf('pub%03d.md',i) '']
+%         'excerpt: ''This paper is about the number 1. The number 2 is left for future work.'''
+%         'date: 2009-10-01'
+%         ['venue: ''' papers.Journal_Book{i} '''']
+%         ['paperurl: ''http://academicpages.github.io/files/paper1.pdf''']
+%         ['citation: ''' papers.Citation{i} '''']
+%         ['authors: ''' papers.AuthorsText{i} '''']
+%         '---'
+%         'This paper is about the number 1. The number 2 is left for future work.'
+%         ''
+%         '[Download paper here](http://academicpages.github.io/files/paper1.pdf)'
+%         ''
+%         'Recommended citation: Your Name, You. (2009). "Paper Title Number 1." <i>Journal 1</i>. 1(1).'
+%         
+%         };
+
+
+  lines ={
         '---'
-        ['title: "' papers.Title{i} '"']
+        'title: "Paper Title Number 1"'
         'collection: publications'
-        ['permalink: /publication/' sprintf('pub%03d.md',i) '']
+        'permalink: /publication/2009-10-01-paper-title-number-1'
         'excerpt: ''This paper is about the number 1. The number 2 is left for future work.'''
         'date: 2009-10-01'
-        ['venue: ''' papers.Journal_Book{i} '''']
-        ['paperurl: ''http://academicpages.github.io/files/paper1.pdf''']
-        ['citation: ''' papers.Citation{i} '''']
-        ['authors: ''' papers.AuthorsText{i} '''']
+        'venue: ''Journal 1'''
+        'paperurl: ''http://academicpages.github.io/files/paper1.pdf'''
+        'citation: ''Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).'''
+        'authors: ''Punugati, Sai A, Tian, Jing, Otero-Millan, Jorge'''
         '---'
         'This paper is about the number 1. The number 2 is left for future work.'
         ''
@@ -24,7 +45,6 @@ for i=1:3%height(papers)
         'Recommended citation: Your Name, You. (2009). "Paper Title Number 1." <i>Journal 1</i>. 1(1).'
         
         };
-
 
     fid = fopen(['C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\ocular-motor-lab.github.io\_publications\' sprintf('pub%03d.md',i)],'w');
     for jj = 1 : size( lines, 1 )
