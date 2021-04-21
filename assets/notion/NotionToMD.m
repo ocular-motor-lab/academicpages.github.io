c1b9efa8bb9bf25844c18d772fdab760dbf491b9
Jorge Otero-Millan <jorge.oteromillan@gmail.com>
2021-04-21 00:32:13 -0700

    test

--- assets/notion/NotionToMD.m
+++ assets/notion/NotionToMD.m
@@ -37,8 +37,9 @@ title = strrep(title,'”','');
         ['date: ' papers.PublicationDate{i}]
         'venue: ''Journal 1'''
         'paperurl: ''http://academicpages.github.io/files/paper1.pdf'''
-        'citation: ''Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).'''
-        'authors: ''Punugati, Sai A, Tian, Jing, Otero-Millan, Jorge'''
+        ['citation: ''' papers.Citation{i} '''']
+        ['authors: ''' papers.AuthorsText{i} '''']
+        ['year: ''' num2str(papers.Year(i)) '''']
         '---'
         'This paper is about the number 1. The number 2 is left for future work.'
         ''
