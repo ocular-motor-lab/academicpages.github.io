babed0783e9d17c6ffde0db68d04039b5b4618b0
Jorge Otero-Millan <jorge.oteromillan@gmail.com>
2021-04-20 23:22:16 -0700

    test

--- assets/notion/NotionToMD.m
+++ assets/notion/NotionToMD.m
@@ -7,15 +7,15 @@ for i=1:3%height(papers)
     i
     lines ={
         '---'
-        'title: "Paper Title Number 1"'
+        ['title: "' papers.Title{i} '"']
         'collection: publications'
-        'permalink: /publication/2009-10-01-paper-title-number-1'
+        ['permalink: /publication/' sprintf('pub%03d.md',i) '']
         'excerpt: ''This paper is about the number 1. The number 2 is left for future work.'''
         'date: 2009-10-01'
-        'venue: ''Journal 1'''
-        'paperurl: ''http://academicpages.github.io/files/paper1.pdf'''
-        'citation: ''Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).'''
-        'authors: ''Punugati, Sai A, Tian, Jing, Otero-Millan, Jorge'''
+        ['venue: ''' papers.Journal_Book{i} '''']
+        ['paperurl: ''http://academicpages.github.io/files/paper1.pdf''']
+        ['citation: ''' papers.Citation{i} '''']
+        ['authors: ''' papers.AuthorsText{i} '''']
         '---'
         'This paper is about the number 1. The number 2 is left for future work.'
         ''
@@ -49,3 +49,24 @@ end
 %         '---'
 %         'Abstract will go here'
 %         ''
+
+% 
+%   lines ={
+%         '---'
+%         'title: "Paper Title Number 1"'
+%         'collection: publications'
+%         'permalink: /publication/2009-10-01-paper-title-number-1'
+%         'excerpt: ''This paper is about the number 1. The number 2 is left for future work.'''
+%         'date: 2009-10-01'
+%         'venue: ''Journal 1'''
+%         'paperurl: ''http://academicpages.github.io/files/paper1.pdf'''
+%         'citation: ''Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).'''
+%         'authors: ''Punugati, Sai A, Tian, Jing, Otero-Millan, Jorge'''
+%         '---'
+%         'This paper is about the number 1. The number 2 is left for future work.'
+%         ''
+%         '[Download paper here](http://academicpages.github.io/files/paper1.pdf)'
+%         ''
+%         'Recommended citation: Your Name, You. (2009). "Paper Title Number 1." <i>Journal 1</i>. 1(1).'
+%         
+%         };
