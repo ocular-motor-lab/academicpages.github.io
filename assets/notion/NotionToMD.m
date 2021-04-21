5f27dbc775dcf7038848f08aca0632dce68ebd84
Jorge Otero-Millan <jorge.oteromillan@gmail.com>
2021-04-21 00:12:44 -0700

    test

--- assets/notion/NotionToMD.m
+++ assets/notion/NotionToMD.m
@@ -28,7 +28,7 @@ for i=1:3%height(papers)
 
   lines ={
         '---'
-        ['title: "' papers.Title{i} '"']
+        ['title: "' 'Paper Title Number 1' '"']
         'collection: publications'
         'permalink: /publication/2009-10-01-paper-title-number-1'
         'excerpt: ''This paper is about the number 1. The number 2 is left for future work.'''
