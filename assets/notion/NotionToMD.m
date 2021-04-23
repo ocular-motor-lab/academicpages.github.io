538c37cf366e68e4293e0b39cab2c4a903a91809
Jorge Otero-Millan <jorge.oteromillan@gmail.com>
2021-04-22 20:46:41 -0700

    t

--- assets/notion/NotionToMD.m
+++ assets/notion/NotionToMD.m
@@ -1,5 +1,5 @@
 %%
-papers = readtable('Publication pipeline 29c020d9669848ab978c1247833d386f.csv');
+papers = readtable('Publication pipeline 29c020d9669848ab978c1247833d386f.csv', 'encoding','UTF-8');
 papers.Status = categorical(papers.Status);
 papers(papers.Status ~= 'Published ' | ismissing(papers.Year),:) = [];
 
@@ -115,6 +115,7 @@ text = strrep(text,'–','-');
 text = strrep(text,'“','');
 text = strrep(text,'”','');
 text = strrep(text,'’','''');
+text = strrep(text,'','fi');
 
 scapes = '\`*_{[(#+-.!';
 
